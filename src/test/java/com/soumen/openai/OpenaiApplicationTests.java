db0c66c963b660a2a2116a8447f4c6dcb61d2d42
soumen <soumenpaloi@gmail.com>
2026-09-08 15:39:19 +0530

    updating properties

--- src/test/java/com/soumen/openai/OpenaiApplicationTests.java
+++ src/test/java/com/soumen/openai/OpenaiApplicationTests.java
@@ -16,7 +16,7 @@ import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @TestPropertySource(properties ={
-		"spring.ai.openai.api-key=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",
+			"spring.ai.openai.api-key=${OPENAI_API_KEY}",
 		"logging.level.org.springframework.ai.chat.client.advisor=DEBUG"
        } )
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
